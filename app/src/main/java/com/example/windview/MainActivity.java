05087ed714401207fb71f0112fe7ba2496993167
AhmadNemati <nematiprog@gmail.com>
2016-12-21 14:03:05 +0330

    add background and transparent status

--- app/src/main/java/com/example/windview/MainActivity.java
+++ app/src/main/java/com/example/windview/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.SeekBar;
 
 import com.github.ahmadnemati.wind.WindView;
 import com.github.ahmadnemati.wind.enums.TrendType;
+import com.mikepenz.materialize.MaterializeBuilder;
 
 public class MainActivity extends AppCompatActivity {
     private WindView windView;
@@ -16,14 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new MaterializeBuilder()
+                .withActivity(this)
+                .withFullscreen(true)
+                .withTranslucentStatusBarProgrammatically(false)
+                .withTintedStatusBar(false).build();
         windView = (WindView) findViewById(R.id.windView);
-        seekBar = (AppCompatSeekBar) findViewById(R.id.seekbar);
         windView.setPressure(50);
         windView.setPressureUnit("in Hg");
         windView.setWindSpeed(15);
         windView.setWindSpeedUnit("Km/s");
         windView.setWindDirection("Salam");
         windView.setTrendType(TrendType.UP);
+
 
 
 
