1729cbb829ff27cb5b15099a2ee399880d8e0bbb
AhmadNemati <nematiprog@gmail.com>
2016-12-24 17:02:15 +0330

    change layout

--- app/src/main/java/com/example/windview/MainActivity.java
+++ app/src/main/java/com/example/windview/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.windview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.SeekBar;
 
 import com.github.ahmadnemati.wind.WindView;
 import com.github.ahmadnemati.wind.enums.TrendType;
 import com.mikepenz.materialize.MaterializeBuilder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
     private WindView windView;
+    private SeekBar windSpeed;
+    private SeekBar pressureSpeed;
 
 
     @Override
@@ -22,17 +24,39 @@ public class MainActivity extends AppCompatActivity {
                 .withTranslucentStatusBarProgrammatically(false)
                 .withTintedStatusBar(false).build();
         windView = (WindView) findViewById(R.id.windview);
-        windView.setPressure(50);
+        windSpeed = (SeekBar) findViewById(R.id.wind_speed);
+        pressureSpeed = (SeekBar) findViewById(R.id.pressure);
+        windView.setPressure(20);
         windView.setPressureUnit("in Hg");
-        windView.setWindSpeed(15);
+        windView.setWindSpeed(1);
         windView.setWindSpeedUnit("Km/s");
-        windView.setWindDirection("Salam");
         windView.setTrendType(TrendType.UP);
         windView.start();
+        windSpeed.setOnSeekBarChangeListener(this);
+        pressureSpeed.setOnSeekBarChangeListener(this);
 
 
+    }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        switch (seekBar.getId()) {
+            case R.id.wind_speed:
+                windView.setWindSpeed(i);
+                break;
+            case R.id.pressure:
+                windView.setPressure(i);
+                break;
+        }
+    }
 
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
 }
